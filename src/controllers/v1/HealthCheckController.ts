5a32107d9689996b91bf61ea7bf1bbcf61f3bda4
Modesto Figuereo <figuereo.fernadez.modesto@gmail.com>
2021-07-31 13:03:06 -0400

    Add health check endpoint.

--- src/controllers/v1/HealthCheckController.ts
+++ src/controllers/v1/HealthCheckController.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from "decorators";
 import HttpResponse from "infrastructure/helpers/HttpResponse";
 import BaseController from "controllers/BaseController";
 
-@Controller('/home')
-export default class HomeController extends BaseController {
+@Controller('/health-check')
+export default class HealthCheckController extends BaseController {
 
   @Get('/')
-  public async index(): Promise<HttpResponse> {
+  public async check(): Promise<HttpResponse> {
     return HttpResponse.ok(null, 'Everything alright!');
   }
 }
