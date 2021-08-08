4f8ff0f4113415c0a21ec81ce6caa1144b860e0c
Modesto Figuereo <figuereo.fernadez.modesto@gmail.com>
2021-08-08 18:43:02 -0400

    Update README.md.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const name = process.env.API_NAME || 'DDD Template';
+export const name = process.env.API_NAME || 'Drapit';
 export const port = parseInt(process.env.API_PORT || '3000', 10);
 export const payloadSize = process.env.API_PAYLOAD_MAX_SIZE;
 export const rootDir = `${process.env.ROOT_DIR}`;
